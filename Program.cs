44a66ce9cc5b1e9d5c6d4a3118e56d7722546b20
Tricia Amheiser <tricia@taarts.net>
2022-05-01 21:04:28 -0400

    Algorithm_B following lesson with GStark as reinforcement of Algorithmic method

--- Program.cs
+++ Program.cs
@@ -1,12 +1,128 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AllCardsOnDeck2
+
+/* this is part of the review to practice more on writing the algorithm */
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            //   Algorithm_A
+            // - Make a list of 52 cards = deck
+            //   var deck = new List<string>()
+            //   {
+            //   "Ace of Spades",
+            //  "2 of Spades",
+            //  "3 of Spades",
+            //  "4 of Spades",
+            //  "5 of Spades",
+            //  "6 of Spades",
+            //  "7 of Spades",
+            //  "8 of Spades",
+            //  "9 of Spades",
+            //  "10 of Spades",
+            //  "Jack of Spades",
+            //  "Queen of Spades",
+            //  "King of Spades",
+            //  "Ace of Hearts",
+            //  "2 of Hearts",
+            //  "3 of Hearts",
+            //  "4 of Hearts",
+            //  "5 of Hearts",
+            //  "6 of Hearts",
+            //  "7 of Hearts",
+            //  "8 of Hearts",
+            //  "9 of Hearts",
+            //  "10 of Hearts",
+            //  "Jack of Hearts",
+            //  "Queen of Hearts",
+            //  "King of Hearts",
+            //   "Ace of Diamonds",
+            //  "2 of Diamonds",
+            //  "3 of Diamonds",
+            //  "4 of Diamonds",
+            //  "5 of Diamonds",
+            //  "6 of Diamonds",
+            //  "7 of Diamonds",
+            //  "8 of Diamonds",
+            //  "9 of Diamonds",
+            //  "10 of Diamonds",
+            //  "Jack of Diamonds",
+            //  "Queen of Diamonds",
+            //  "King of Diamonds",
+            //  "Ace of Clubs",
+            //  "2 of Clubs",
+            //  "3 of Clubs",
+            //  "4 of Clubs",
+            //  "5 of Clubs",
+            //  "6 of Clubs",
+            //  "7 of Clubs",
+            //  "8 of Clubs",
+            //  "9 of Clubs",
+            //  "10 of Clubs",
+            //  "Jack of Clubs",
+            //  "Queen of Clubs",
+            //  "King of Clubs",
+            //   };
+
+            // Algorithm_B 
+            // - Make a list of the four suits and call it "suits' 
+            var suits = new List<string>() {
+              "Clubs", "Diamonds", "Hearts", "Spades"
+            };
+            // - make a list of the 13 faces and call them "faces" 
+            var faces = new List<string>() {
+            "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"
+            };
+            // - make a list of the two strings together and all it "deck" 
+            var deck = new List<string>();
+            //   - make a loop that goes through the list "suits" 
+            foreach (var suit in suits)
+            {
+                //     - make a list that goes through the list "faces" 
+                foreach (var face in faces)
+                {
+                    //     - for each face, make a new string, format CS $"{face} of {suit}"
+                    var card = $"{face} of {suit}";
+                    //     - add that new string to the end of the deck list 
+                    deck.Add(card);
+                }
+            }
+
+            /* ------------------------------------------------------------------*/
+            // - Shuffle those cards according to the Fisher Yates
+            //     numberOfCards = length of our deck
+            var numberOfCards = deck.Count;
+            //     for rightIndex from numberOfCards - 1 down to 1 do:
+            for (var rightIndex = numberOfCards - 1; rightIndex >= 1; rightIndex--)
+            {
+                //     leftIndex = random integer that is greater than or equal to 0 and LESS than rightIndex. (See the section "How do we get a random integer")
+                var randomNumberGenerator = new Random();
+                var leftIndex = randomNumberGenerator.Next(rightIndex);
+                //     Now swap the values at rightIndex and leftIndex by doing this:
+                //     leftCard = the value from deck[leftIndex]
+                var leftCard = deck[leftIndex];
+                //     rightCard = the value from deck[rightIndex]
+                var rightCard = deck[rightIndex];
+                //     deck[rightIndex] = leftCard
+                deck[rightIndex] = leftCard;
+                //     deck[leftIndex] = rightCard
+                deck[leftIndex] = rightCard;
+            }
+
+            Console.WriteLine("Done shuffling");
+            // - first card = deck[0]
+            var firstCard = deck[0];
+            // - second card = deck[1]
+            var secondCard = deck[1];
+            // - print first and second cards
+            Console.WriteLine($"The first card is {firstCard}");
+            Console.WriteLine($"The second card card is {secondCard}");
+
+
+
+        }
     }
-  }
 }
--- README.md
+++ README.md
@@ -1,1 +1,72 @@
 # AllCardsOnDeck2
+
+PEDAC:
+Problem
+Example
+Data (Data structures)
+Algorithm
+Code
+
+Problem: Explorer Mode
+
+Deck has 52 - a list with 52 elements that is made from the two strings detailed below
+4 suits - a list with 4 elements
+each "face" is A, 2, 3,... J, Q, K. - a list with 13 elements
+the last two strings have to be explicitly in the document for the first string to be able to run.
+
+When the deck is created and you can show each string as "Ace of Hearts", "Jack of Clubs", "2 of..." then create a method to shuffle the cards, using Fisher Yates.
+Then show the first two cards after the shuffle.
+
+#ExampleData:
+
+    Ace of Spades
+    2 of Spades
+    3 of Spades
+    4 of Spades
+    5 of Spades
+    6 of Spades
+    7 of Spades
+    8 of Spades
+    9 of Spades
+    10 of Spades
+    Jack of Spades
+    Queen of Spades
+    King of Spades
+    ...
+    "- There are four suits: "Clubs", "Diamonds", "Hearts", and "Spades".
+    There are 13 ranks: "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", and "King". -"
+
+Data (Data Structure)
+
+    'string'
+    'list'
+    loops
+
+/_ this whole process should taken one - two hours _/
+
+Algorithm_A - Make a list of 52 cards = deck - make 52 distinct cards from the #ExampleData - Shuffle those cards according to the Fisher Yates
+numberOfCards = length of our deck
+
+        for rightIndex from numberOfCards - 1 down to 1 do:
+        leftIndex = random integer that is greater than or equal to 0 and LESS than rightIndex. (See the section "How do we get a random integer")
+
+        Now swap the values at rightIndex and leftIndex by doing this:
+        leftCard = the value from deck[leftIndex]
+        rightCard = the value from deck[rightIndex]
+        deck[rightIndex] = leftCard
+        deck[leftIndex] = rightCard
+    - first card = deck[0]
+    - second card = deck[1]
+    - print first and second cards
+
+Algorithm_B
+
+- Make a list of the four suits and call it "suits'
+- make a list of the 13 faces and call them "faces"
+- make a list of the two strings together and all it "deck"
+  - make a loop that goes through the list "suits"
+    - make a list that goes through the list "faces"
+    - for each face, make a new string
+    - format CS $"{face} of {suit}"
+    - add that new string to the end of the deck list
+- same as Algorithm_A
